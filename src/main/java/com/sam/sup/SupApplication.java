52382199ce19a1eb689b2b9b7c1535fc3dac1156
Sam <doansamquoc@gmail.com>
2025-12-07 07:53:18 +0700

    Initial project

---
+++ src/main/java/com/sam/sup/SupApplication.java
@@ -0,0 +1,13 @@
+package com.sam.sup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sam/sup/SupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.sup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
