141df958f20ddf2bc5ce4e527d4f7b906ae406dc
Sam <doansamquoc@gmail.com>
2025-12-25 21:50:17 +0700

    feat(constant): add forbidden error code

--- src/main/java/com/sam/sup/core/enums/ErrorCode.java
+++ src/main/java/com/sam/sup/core/enums/ErrorCode.java
@@ -10,15 +10,16 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum ErrorCode {
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Unauthorized"),
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Invalid credentials"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email already exists"),
-    USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
-    DISPLAY_NAME_CANNOT_BLANK(HttpStatus.BAD_REQUEST, "Display name cannot blank");
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Unauthorized"),
+  INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+  INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "Invalid credentials"),
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Invalid token"),
+  EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "Email already exists"),
+  USERNAME_ALREADY_EXISTS(HttpStatus.CONFLICT, "Username already exists"),
+  DISPLAY_NAME_CANNOT_BLANK(HttpStatus.BAD_REQUEST, "Display name cannot blank"),
+  ACCESS_DENIED(HttpStatus.FORBIDDEN, "Access Denied: You don't have permission");
 
-    HttpStatus httpStatus;
-    String message;
+  HttpStatus httpStatus;
+  String message;
 }
