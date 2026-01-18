62a7f10f7b270ec5e4454ca2e585cf21ab23d07a
Sam <doansamquoc@gmail.com>
2026-01-19 03:57:40 +0700

    chore: removed lib imported unused

--- src/main/java/com/sam/sup/modules/auth/controller/AuthController.java
+++ src/main/java/com/sam/sup/modules/auth/controller/AuthController.java
@@ -24,8 +24,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
